2ab010430cd6b9c1117d8268bc390d963040d370
Albert Lahat <lahat.albert@gmail.com>
2015-02-20 12:28:16 +0000

    improved plots

--- plot.py
+++ plot.py
@@ -1,11 +1,16 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import pylab
+
 import glob
 
-def plotDIR(dir):
+def plotDIR(dir,distortion=3):
 	''' pieplots all the TSVs in that directory '''
 	files = glob.glob(dir+'/*.tsv')
 	for f in files:
-		pieplot(f)
+		pieplot(f,distortion=distortion)
+		#colplot(f)
 		
 def wrap(txt, linelength=15):
 	'''turns a line of text into a wraped block'''
@@ -27,8 +32,26 @@ def color(txt):
 	g = float(h[-5:-3])/100
 	b = float(h[-7:-5])/100
 	return r,g,b
+def readTSV(tsv,wraplabel=10):
+
+	f = open(tsv,'r')
+	exon = f.read()
+	f.close()
+	lines = exon.splitlines()
+	head,data = lines[:2], lines[2:]
+	head ='\n'.join(head)
+	vals , labels, colors = [],[],[]
+	for line in data:
+		label,val = line.split('\t')
+		val = float(val)
+		if val > 0:
+			c = color(label)
+			colors.append(c)
+			labels.append(wrap(label.replace('_',' ')+' '+str(round(val,2)),wraplabel))
+			vals.append(val)
+	return vals,labels,colors
 	
-def pieplot(tsv,show=False):
+def pieplot(tsv,show=False,distortion=3,wraplabel=10):
 	'''pieplots atsv file with a header'''
 	f = open(tsv,'r')
 	exon = f.read()
@@ -41,17 +64,39 @@ def pieplot(tsv,show=False):
 		label,val = line.split('\t')
 		val = float(val)
 		if val > 0:
-			vals.append(val)
 			c = color(label)
 			colors.append(c)
-			labels.append(wrap(label.replace('_',' ')+' '+str(round(val,2)),10))
-	plt.close('all')
-	plt.figure(figsize=(13,13))
-	ax = plt.axes(aspect=1)
-	#f, ax = plt.subplots(figsize(10,10))
-	plt.pie(vals, labels=labels,colors=colors,)
+			labels.append(wrap(label.replace('_',' ')+' '+str(round(val,2)),wraplabel))
+			vals.append(val**distortion)
+	pylab.close('all')
+	#pylab.figure(figsize=(13,13))
+	ax = pylab.axes(aspect=1)
+	#f, ax = pylab.subplots(figsize(10,10))
+	pylab.pie(vals, labels=labels,colors=colors,)
 	ax.set_title(head)
-	fname = tsv[:-3]+'png'
-	plt.savefig(fname)
-	if show:plt.show()
+	fname = tsv[:-4]+'_pie.png'
+	pylab.savefig(fname)
+	if show:pylab.show()
 	
+def colplot(tsv):
+	vals,labels,colors = readTSV(tsv,wraplabel=1000)
+	pylab.close('all')
+	pylab.figure(figsize=[.7+len(vals),10])
+	ax = pylab.axes()
+	index = np.arange(len(vals))
+	barwidth = 1
+	ax.bar(index,
+		vals,
+		width=barwidth,
+		)
+	pylab.xticks(index+barwidth,labels,rotation=90)
+	fname = tsv[:-4]+'_bar.png'
+	pylab.savefig(fname,dpi=200)
+
+def main():
+	import sys
+	dir = sys.argv[-2]
+	distortion = sys.argv[-1]
+	plotDIR(dir)
+
+if __name__=="__main__": main()
