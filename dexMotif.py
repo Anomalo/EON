589d25df93fe117ec3391ca4b31563338bdb9c6c
Anomalo <lahat.albert@gmail.com>
2015-10-14 17:37:38 +0100

    works using prosite scan

--- dexMotif.py
+++ dexMotif.py
@@ -1,32 +1,22 @@
 ##!/usr/bin/env python2.7
 import os
 from optparse import OptionParser
-from eon import obo
+import csv
 import glob
+from eon import fa
 def check_files(taxon='mus musculus',dir = 'annotations/'):
 	'''
 	checks, downloads and installs annotation files
 	'''
 	try: os.makedirs(dir)
 	except OSError: pass
-	#check gmt
-	f = glob.glob(dir+'*.gmt')
-	if f == []: 
-		from eon import go
-		go.getGMT(taxon = taxon, dir = dir)
-		reload(go)
+
 	#check gtf
 	f = glob.glob(dir+'*.gtf')
 	if f == []: 
 		from eon import gtf
 		gtf.getGTF(taxon = taxon, dir = dir)
 		reload(gtf)
-	#check obo
-	f = glob.glob(dir+'*.obo')
-	if f == []: 
-		from eon import obo
-		obo.getOBO(dir = dir)
-		reload(obo)
 	
 		
 def main():
@@ -37,82 +27,46 @@ def main():
 	gene ontology category would be present in other genes within that GO category.
 	'''.replace('\n','').replace('\t','')
 	parser = OptionParser(description=description)
-	parser.add_option('-G','--gene',
+	parser.add_option('-f','--file',
 				action='store',type='string',
-				dest='gene',default='',
-				help='analyze the given gene isoform (DMD-001)')
-	parser.add_option('-e','--exons',
-				action='store',type='string',
-				dest='exons',default='',
-				help='exons to blast, default is all, just add exon number separated by commas, or just two exons separated with Hyphen, 3-7 is the same as 3,4,5,6,7 ')
+				dest='dexFile',default='',
+				help='dexseq output file to analyze')
+
 	parser.add_option('-A','--annotation',
 				action='store', type='string',
 				dest='annotations',default='',
 				help='downloads and generates anotation files of defined taxon -A "mus musculus"')
+
 	parser.add_option('-p','--purge',
 				action='store_true',
 				dest='purge', default=False,
 				help='delete all anotation files (usefull for changing the taxon of interest)')
 	
-	parser.add_option('-f','--file',
-				action='store',type='string',
-				dest='input',default='',
-				help='chose a file with gene names to analyize')
 	parser.add_option('-v','--verbose',
 				action='store_true',
 				dest ='v', default=False,
 				help='shows you what am I thinking')
-	parser.add_option('-s','--wordsize',
-				action='store', type='int',
-				dest='ws',default=11,
-				help='nmer size for blasting (default is 11)')
+
 	parser.add_option('-o','--output',
 				action='store', type='string',
 				dest='output',default='results',
 				help='directory where to save the results')
-	parser.add_option('-B','--bsub',
-				action='store_true',
-				dest='bsub',default=False,
-				help='sends individual gene glasting to its own bsub')
-	parser.add_option('-b','--bsubOptions',
-				action='store', type='string',
-				dest='bsubOptions',default='',
-				help='options to add to bsub in a string format')
-	parser.add_option('-l','--level',
+
+	parser.add_option('-n','--fastaline',
 				action='store', type='int',
-				dest='level', default=3,
-				help='the go level to analyze with with 1 being the roots of the go tree')
-	parser.add_option('-L','--levelALL',
-				action='store_true',
-				dest = 'L',default=True,
-				help='Analize all go levels')
-	parser.add_option('-d','--distortion',
-				action='store',type='int',
-				dest='distortion',default=3,
-				help='integer by which to raise the blasting scores when plotting pie charts')
+				dest='fastaline',default=80,
+				help='line length for fasta files')
+
+	parser.add_option('-P','--prosite',
+				action='store', type='string',
+				dest='prosite',default='annotations/prosite.dat',
+				help='prosite.dat file')
+	
 	(options, args) = parser.parse_args()
 	v = options.v
-	ws=options.ws
-	level=options.level
-	bsub=options.bsub
-	bsuboptions=options.bsubOptions
 	output = options.output
 	annotations = options.annotations
-	distortion = options.distortion
-	L = options.L
-	exons = options.exons
-
-	#parsing exons
-	if '-' in exons: 
-		exons = exons.split('-')
-		if len(exons) != 2:
-			raise  ValueError('Could not parse exons')
-		first, last = exons
-		exons = range(int(first),int(last)+1)
-	else:
-		exons = exons.split(',')
-	if exons == ['']:exons = []
-	#
+	prosite = options.prosite
 	if options.purge:
 		commandOptions ='-rf'
 		if v:commandOptions+='v'
@@ -120,44 +74,57 @@ def main():
 	
 	if annotations != '':
 		check_files(taxon = annotations)
-	genes = []
-	if options.gene != '':
-		genes = genes +  options.gene.split()
-	if options.input != '':
-		f = open(options.input,'r')
-		genesf = f.read()
-		f.close()
-		genes = genes + genesf.split()
 	
-	if genes!=[]:
-		if v:print 'will glast the exons of: ',', '.join(genes)
-		OBO = obo.OBO('annotations/go.obo')
-		gmt = glob.glob('annotations/*.gmt')[0]
-		if not L:
-			gmt = OBO.filterGMT(gmt,level = level)
-		if not bsub: 
-			from eon import blastgo
+	tempFasta ,ids= dexSeqToFasta(options.dexFile,verbose = v,linelength=options.fastaline)
+	if v:print tempFasta	
+	prositeCMD = 'perl ps_scan/ps_scan.pl -e %(id)s -d %(prosite)s %(tempFasta)s > %(tempFasta)s_%(id)s.prosite '
+	for id in ids:
 
-			g = blastgo.glast(gmt,v=v,output=output,ws=ws,distortion=distortion)
-		for gene in genes:
-			if bsub:
-				cmd = ' '.join(['bsub',
-						bsuboptions,
-						'"./eon.py',
-						'-G',gene,
-						'-ws',str(ws),
-						'-o',output,
-						'-d',distortion,
-						'"'
-						])
-				print cmd
-				os.system(cmd)
-						
-						
-			else:
-				g.glastGene(gene,exonsToBlast=exons)
-		if v:print 'DONE'
-				
+		if v:print prositeCMD % locals()
+		os.system(prositeCMD % locals())
+	
+	
+
+	prositeToDexseq()
+def prositeToDexseq(dexseq,dexseqOut):
+	pass
+def dexSeqToFasta(dexseq,sep=',',verbose = False,linelength=80):
+	'''
+	reads a dexseq output and produces a fasta file based on the coordinates of sequences altered
+	returns the filename of the fasta produced and a list of ids
+	'''
+	f = open(dexseq)
+	csvfile = csv.reader(f,delimiter=sep)
+	n=1
+	fasta = []
+	ids = []
+	for row in csvfile:
+		if n==1:
+			header = row
+			n+=1
+			continue
+		rowD = dict(zip(header,row))	
+		n+=1
+		seqname	= rowD['genomicData.seqnames']
+		strand  = rowD['genomicData.strand']
+		start   = rowD['genomicData.start']
+		end     = rowD['genomicData.end']
+		gene    = rowD['gene']
+		seq     = fa.seq_coords(seqname,start,end,strand)
+		choppedSeq = ''
+		for i in range(0,len(seq),linelength):
+			choppedSeq+=seq[i:i+linelength]+'\n'
+		ID = '%(n)s|%(gene)s_%(seqname)s:%(start)s-%(end)s_%(strand)s' % locals()
+		newFasta= '>%(ID)s\n%(choppedSeq)s' % locals()
+		fasta.append(newFasta)
+		ids.append(ID)
+	f.close()
+	fasta = ''.join(fasta)
+	fastaFname = '%(dexseq)s.tmp.fasta'%locals()
+	f = open(fastaFname,'w')
+	f.write(fasta)
+	f.close()
+	return fastaFname,ids
 
 if __name__ == '__main__': 
 	#check_files()
@@ -168,4 +135,3 @@ if __name__ == '__main__':
                 except Exception as e: print e
 
 '''
-
